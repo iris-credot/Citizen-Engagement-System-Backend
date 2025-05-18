e4c63ba3d497b4fb86f9d006dc58ca7b794fdd8d
iris-credot <tetairiscredot@gmail.com>
2025-05-18 21:31:48 +0200

    Added cors

--- Routes/ComplaintRoute.js
+++ Routes/ComplaintRoute.js
@@ -7,7 +7,7 @@ router.post('/create',auth.AuthJWT, complaintController.createComplaint);
 router.get('/getall', auth.superAdminJWT,complaintController.getAllComplaints);
 router.get('/filter',auth.AuthJWT, complaintController.filterComplaints);
 router.get('/getOne/agencyId', auth.AuthJWT,complaintController.getComplaintsByAgency);
-router.get('/getOne/:userId', auth.BothJWT,complaintController.getComplaintsByUser);
+router.get('/getOne/:userId', auth.AuthJWT,complaintController.getComplaintsByUser);
 router.put('/status/:id', auth.BothJWT, complaintController.changeComplaintStatus);
 router.get('/getOne/:id',auth.AuthJWT, complaintController.getComplaintById);
 router.put('/update/:id',auth.AuthJWT, complaintController.updateComplaint);
