f27cf9476d5b46863bdf7284c82c78ac9dd38252
iris-credot <tetairiscredot@gmail.com>
2025-05-21 14:08:04 +0200

    Constructed  the complaint

--- Routes/ComplaintRoute.js
+++ Routes/ComplaintRoute.js
@@ -6,11 +6,11 @@ const complaintController = require('../Controllers/ComplaintController');
 router.post('/create',auth.AuthJWT, complaintController.createComplaint);
 router.get('/getall', auth.superAdminJWT,complaintController.getAllComplaints);
 router.get('/filter',auth.AuthJWT, complaintController.filterComplaints);
-router.get('/getOne/agencyId', auth.AuthJWT,complaintController.getComplaintsByAgency);
+router.get('/getOneAgency/agencyId', auth.AuthJWT,complaintController.getComplaintsByAgency);
 router.get('/getCategories', auth.AuthJWT,complaintController.getComplaintCategories);
 router.get('/getOne/:userId', auth.AuthJWT,complaintController.getComplaintsByUser);
 router.put('/status/:id', auth.BothJWT, complaintController.changeComplaintStatus);
-router.get('/getOne/:id',auth.AuthJWT, complaintController.getComplaintById);
+router.get('/getOneComplaint/:id',auth.AuthJWT, complaintController.getComplaintById);
 router.put('/update/:id',auth.AuthJWT, complaintController.updateComplaint);
 router.delete('/delete/:id', auth.AuthJWT,complaintController.deleteComplaint);
 
